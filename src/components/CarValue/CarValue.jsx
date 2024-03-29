4fd33a5d39e2ab2f67bbac6e4240dbc7709fcd6a
Dan Moffat <danmoffat@gmail.com>
2024-03-29 00:01:19 +0000

    Added logic to show total value of cars

--- src/components/CarValue/CarValue.jsx
+++ src/components/CarValue/CarValue.jsx
@@ -1,7 +1,11 @@
+import { useSelector } from 'react-redux';
+
 const CarValue = () => {
+  const cars = useSelector(({ cars }) => cars);
+
   return (
     <div className='bg-green-100 p-4 rounded text-end'>
-      <h2 className='font-semibold text-2xl'> Total Value: £00000</h2>
+      <h2 className='font-semibold text-2xl'>Total Value: £{cars.reduce((acc, car) => acc + car.carValue, 0)}</h2>
     </div>
   );
 };
