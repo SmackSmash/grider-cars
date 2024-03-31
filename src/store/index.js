a199cdac37b6a10953e10c80de3b98a04fd76ab7
Dan Moffat <danmoffat@gmail.com>
2024-03-31 22:59:17 +0100

    Restructured index.js to make it more scalable

--- src/store/index.js
+++ src/store/index.js
@@ -2,11 +2,12 @@ import { configureStore } from '@reduxjs/toolkit';
 
 import form from './slices/formSlice';
 import { setName, setCost } from './slices/formSlice';
+export { setName, setCost };
 
 import cars from './slices/carsSlice';
 import { setSearchTerm, addCar, removeCar } from './slices/carsSlice';
+export { setSearchTerm, addCar, removeCar };
 
 const store = configureStore({ reducer: { form, cars } });
 
-export { setName, setCost, setSearchTerm, addCar, removeCar };
 export default store;
