5b9a423adfe1b3ecc5e40b8f6338ec76d0850e92
Dan Moffat <danmoffat@gmail.com>
2024-04-01 22:52:56 +0100

    Removed unneccesary variable to filter list

--- src/components/CarList/CarList.jsx
+++ src/components/CarList/CarList.jsx
@@ -12,26 +12,28 @@ const CarList = () => {
     dispatch(removeCar(id));
   };
 
-  const filteredList = list.filter(({ name }) => name.includes(searchTerm));
-
   return (
     <div className='bg-slate-400 p-4 rounded mb-4 flex flex-col gap-3'>
-      {filteredList.length
-        ? filteredList.map(({ name, cost, id }) => (
-            <div className='bg-slate-200 px-4 py-2 rounded shadow-sm flex justify-between items-center' key={id}>
-              <p>
-                {name.includes(nameToAdd) && nameToAdd !== '' ? name.slice(0, name.indexOf(nameToAdd)) : name}
-                {name.includes(nameToAdd) && nameToAdd !== '' && <strong>{nameToAdd}</strong>}
-                {name.includes(nameToAdd) && nameToAdd !== '' && name.slice(name.indexOf(nameToAdd) + nameToAdd.length)}
-                - £{cost.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',')}
-              </p>
-              <button
-                className='flex-none bg-red-700 px-6 py-1 rounded text-slate-200 font-bold uppercase pointer shadow-sm hover:bg-red-600 active:relative active:top-[1px]'
-                onClick={() => handleClick(id)}>
-                Delete
-              </button>
-            </div>
-          ))
+      {list.length
+        ? list
+            .filter(({ name }) => name.includes(searchTerm))
+            .map(({ name, cost, id }) => (
+              <div className='bg-slate-200 px-4 py-2 rounded shadow-sm flex justify-between items-center' key={id}>
+                <p>
+                  {name.includes(nameToAdd) && nameToAdd !== '' ? name.slice(0, name.indexOf(nameToAdd)) : name}
+                  {name.includes(nameToAdd) && nameToAdd !== '' && <strong>{nameToAdd}</strong>}
+                  {name.includes(nameToAdd) &&
+                    nameToAdd !== '' &&
+                    name.slice(name.indexOf(nameToAdd) + nameToAdd.length)}
+                  - £{cost.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',')}
+                </p>
+                <button
+                  className='flex-none bg-red-700 px-6 py-1 rounded text-slate-200 font-bold uppercase pointer shadow-sm hover:bg-red-600 active:relative active:top-[1px]'
+                  onClick={() => handleClick(id)}>
+                  Delete
+                </button>
+              </div>
+            ))
         : 'No cars in list'}
     </div>
   );
