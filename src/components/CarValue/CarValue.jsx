bfdea70be27016362039bad15749fec6e8792233
Dan Moffat <danmoffat@gmail.com>
2024-04-02 20:03:34 +0100

    Added function to filter car prices and only show updated price

--- src/components/CarValue/CarValue.jsx
+++ src/components/CarValue/CarValue.jsx
@@ -2,12 +2,14 @@ import { useSelector } from 'react-redux';
 
 const CarValue = () => {
   const list = useSelector(({ cars: { list } }) => list);
+  const searchTerm = useSelector(({ cars: { searchTerm } }) => searchTerm);
 
   return (
     <div className='bg-green-100 p-4 rounded text-end'>
       <h2 className='font-semibold text-2xl'>
         Total Value: £
         {list
+          .filter(({ name }) => name.includes(searchTerm))
           .reduce((acc, car) => acc + car.cost, 0)
           .toString()
           .replace(/\B(?=(\d{3})+(?!\d))/g, ',')}
